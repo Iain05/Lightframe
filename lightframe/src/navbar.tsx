6c7fc57824076bba4691ca69dbabf8a9e79bf5ab
Iain05 <58150364+Iain05@users.noreply.github.com>
2025-06-19 22:20:00 -0700

    added a crappy menu bar, next need to have pages for collections

---
+++ lightframe/src/navbar.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import './css/navbar.css';
+
+const Navbar = () => {
+    const [showNav, setShowNav] = useState(false);
+
+    const handleShowNavbar = () => {
+        setShowNav(!showNav)
+    }
+    return (
+        <nav className="navbar">
+            <div className="container">
+                <div className="logo">
+                    Iain Griesdale
+                </div>
+                <div className='menu-icon' onClick={handleShowNavbar}>
+                    <MenuRoundedIcon />
+                </div>
+                <div className={`nav-elements ${showNav && 'active'}`}>
+                    <ul>
+                        <li><NavLink to="/" className="nav-link" onClick={() => setShowNav(false)}>Portfolio</NavLink></li>
+                        <li><NavLink to="/events" className="nav-link" onClick={() => setShowNav(false)}>Events</NavLink></li>
+                        <li><NavLink to="/portraits" className="nav-link" onClick={() => setShowNav(false)}>Portraits</NavLink></li>
+                        <li style={{ paddingRight: '30px' }}><NavLink to="/contact" className="nav-link" onClick={() => setShowNav(false)}>Contact</NavLink></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
