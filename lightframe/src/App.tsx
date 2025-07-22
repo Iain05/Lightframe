b6da9665c6658ef5ee40e358f825fc2b6323243e
Iain05 <58150364+Iain05@users.noreply.github.com>
2025-07-22 14:25:38 -0700

    added google analytics hopefully

--- lightframe/src/App.tsx
+++ lightframe/src/App.tsx
@@ -2,6 +2,8 @@ import './css/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useAutoLogout } from './hooks/use-auto-logout';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import Navbar from './navbar';
 import Footer from './footer';
@@ -11,12 +13,28 @@ import Login from './auth/login';
 import About from '@src/pages/about';
 import Home from './pages/home';
 
+declare global {
+    interface Window {
+        gtag?: (command: string, targetId: string, config?: { [key: string]: string | number | boolean }) => void;
+    }
+}
+
 const queryClient = new QueryClient();
 
 function App() {
+    const location = useLocation();
+
     // Check for token expiration every 5 minutes
     useAutoLogout(5);
-    
+
+    useEffect(() => {
+        if (window.gtag) {
+            window.gtag('config', `${import.meta.env.VITE_GA_ID}`, {
+                page_path: location.pathname,
+            });
+        }
+    }, [location]);
+
     return (
         <QueryClientProvider client={queryClient}>
             <div className="App">
